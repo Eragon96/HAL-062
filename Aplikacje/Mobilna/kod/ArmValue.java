37d8276ce6ce0e6a0c175b4f50773858382bbef9
PrzygoKNR <jakubprzygodzki@gmail.com>
2018-04-15 18:18:13 +0200

    dodanie kodów źródłowych aplikacji mobilnej

---
+++ Aplikacje/Mobilna/kod/ArmValue.java
@@ -0,0 +1,10 @@
+package com.example.t460s.hal_062;
+
+/**
+ * Created by T460s on 29.03.2018.
+ */
+
+public class ArmValue {
+    static public int[] values = {0,0,0,0,0,0,0};
+    static public void setValue(int tag, int input){ values[tag] = input; }
+}
